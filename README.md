dae81d4f830b13032fedc642f3a7c02ae43fa3c6
Duncan Sparrell <duncan@sfractal.com>
2016-09-10 23:40:06 -0400

    startup
    used rebar3 to start, added some stuff and realized I should be
    tracking so initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+ocas
+=====
+
+ocas - plural of oca, a edible tuber of Oxalis tuberosa,a wood sorrel of the Andes
+or maybe it's an acronym OCAS - OpenC2 API Simulator.
+
+Ocas is an OTP application written in erlang to:
+- prove out the OpenC2 spec (see blah blah X)
+- be a viable simulator for testing OpenC2 code and scenarios
+- be a template for developing actual openc2 applications (ie replace the simulator code with code to actually perform the real function)
+
+License
+-----
+Copyright 2016 sFractal Consulting
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Build
+-----
+
+    $ rebar3 compile
+
+Build & Test
+-----
+
+    $ rebar3 ct
+
+Organization of this software
+-----
+
+put stuff here
+
+Examples
+-----
+
+put stuff here
---
+++ test/simple_post_SUITE.erl
@@ -0,0 +1,74 @@
+%%%-------------------------------------------------------------------
+%%% Copyright (c) 2016, sFractal Consulting, LLC
+
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+
+%%%-------------------------------------------------------------------
+
+%%%-------------------------------------------------------------------
+%% @doc test simple post
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(simple_post_SUITE).
+-author("Duncan Sparrell").
+-copyright("2016, sFractal Consulting, LLC").
+-license(apache2).
+
+%% for test export all functions
+-compile(export_all).
+
+%% required for common_test to work
+-include_lib("common_test/include/ct.hrl").
+
+%% tests to run
+all() ->
+    [ test_post
+    , to_do
+    ].
+
+%% timeout if no reply in a minute
+suite() ->
+    [{timetrap,{minutes,2}}].
+
+%% setup config parameters
+init_per_suite(Config) ->
+    {ok, _AppList} = application:ensure_all_started(shotgun),
+    {ok, _AppList2} = application:ensure_all_started(ocas),
+
+
+    Config.
+
+test_post(_Config) ->
+    MyPort = application:get_env(ocas, port, 8080),
+    {ok, Conn} = shotgun:open("localhost", MyPort),
+    Headers = [ {<<"content-type">>,<<"application/json">>} ],
+    SomeData = #{ fractalAlg => julian
+                , colorAlg => simplest
+                },
+    Body = jsx:encode(SomeData),
+    Options = #{},
+    {ok, Response} = shotgun:post(Conn, "/sFractal", Headers, Body, Options),
+    #{ status_code := 201, headers := RespHeaders } = Response,
+    %% test header contents are correct
+    { <<"server">>, <<"Cowboy">>} =  lists:keyfind(<<"server">>, 1, RespHeaders),
+    { <<"date">>, _Date } =  lists:keyfind(<<"date">>, 1, RespHeaders),
+    { <<"content-length">>, <<"0">>} =  lists:keyfind(<<"content-length">>, 1, RespHeaders),
+    { <<"content-type">>, <<"text/html">>} =  lists:keyfind(<<"content-type">>, 1, RespHeaders),
+
+    ok.
+
+to_do(_Config) ->
+    {ok, 'cowlib/src/cow_multipart.erl:392: Warning: crypto:rand_bytes/1 is deprecated and will be removed in a future release; use crypto:strong_rand_bytes/1' } = 'cowlib/src/cow_multipart.erl:392: Warning: crypto:rand_bytes/1 is deprecated and will be removed in a future release; use crypto:strong_rand_bytes/1',
+
+    ok.
