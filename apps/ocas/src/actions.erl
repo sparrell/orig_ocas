f340acf571f664913c9b99433d9693cfb6d01b00
Duncan Sparrell <duncan@sfractal.com>
2016-10-02 16:56:25 -0400

    Interim - refactored actions
    haven't finished but saving where at.

---
+++ apps/ocas/src/actions.erl
@@ -0,0 +1,278 @@
+-module(actions).
+%%%-------------------------------------------------------------------
+%%% @author Duncan Sparrell
+%%% @copyright (C) 2015, sFractal Consulting LLC
+%%%
+%%% All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions are
+%%% met:
+%%%
+%%% * Redistributions of source code must retain the above copyright
+%%%   notice, this list of conditions and the following disclaimer.
+%%%
+%%% * Redistributions in binary form must reproduce the above copyright
+%%%   notice, this list of conditions and the following disclaimer in the
+%%%   documentation and/or other materials provided with the distribution.
+%%%
+%%% * The names of its contributors may not be used to endorse or promote
+%%%   products derived from this software without specific prior written
+%%%   permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%%% A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%%% OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%%% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+%%% LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES. LOSS OF USE,
+%%% DATA, OR PROFITS. OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+%%% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%%-------------------------------------------------------------------
+
+-author("Duncan Sparrell").
+-license("Apache 2.0").
+
+-export([ get_valid_action/1
+        , scan/2
+        , locate/2
+        , query/2
+        , report/2
+        , get/2
+        , notify/2
+        , deny/2
+        , contain/2
+        , allow/2
+        , start/2
+        , stop/2
+        , restart/2
+        , pause/2
+        , resume/2
+        , cancel/2
+        , set/2
+        , update/2
+        , move/2
+        , redirect/2
+        , delete/2
+        , snapshot/2
+        , detonate/2
+        , restore/2
+        , save/2
+        , modify/2
+        , throttle/2
+        , delay/2
+        , substitute/2
+        , copy/2
+        , sync/2
+        , distill/2
+        , augment/2
+        , investigate/2
+        , mitigate/2
+        , remediate/2
+        , response/2
+        , alert/2
+        ]).
+
+get_valid_action(Action) ->
+    %% this routine verifies action is on list of valid actions
+    %% and returns atom which is routine to run.
+
+    ValidActions = 
+        #{ <<"scan">> => scan 
+        ,  <<"locate">> => locate 
+        ,  <<"query">> => query 
+        ,  <<"report">> => report 
+        ,  <<"get">> => get 
+        ,  <<"notify">> => notify 
+        ,  <<"deny">> => deny 
+        ,  <<"contain">> => contain 
+        ,  <<"allow">> => allow 
+        ,  <<"start">> => start 
+        ,  <<"stop">> => stop 
+        ,  <<"restart">> => restart 
+        ,  <<"pause">> => pause 
+        ,  <<"resume">> => resume 
+        ,  <<"cancel">> => cancel 
+        ,  <<"set">> => set 
+        ,  <<"update">> => update 
+        ,  <<"move">> => move 
+        ,  <<"redirect">> => redirect 
+        ,  <<"delete">> => delete 
+        ,  <<"snapshot">> => snapshot 
+        ,  <<"detonate">> => detonate 
+        ,  <<"restore">> => restore 
+        ,  <<"save">> => save 
+        ,  <<"modify">> => modify 
+        ,  <<"throttle">> => throttle 
+        ,  <<"delay">> => delay 
+        ,  <<"substitute">> => substitute 
+        ,  <<"copy">> => copy 
+        ,  <<"sync">> => sync 
+        ,  <<"distill">> => distill 
+        ,  <<"augment">> => augment 
+        ,  <<"investigate">> => investigate 
+        ,  <<"mitigate">> => mitigate 
+        ,  <<"remediate">> => remediate 
+        ,  <<"response">> => response 
+        ,  <<"alert">> => alert 
+        }, 
+
+    %% return ActionAtom if valid action, otherwise return undefined
+    maps:get(Action, ValidActions, undefined).
+
+%% This routine API handles all the actions that can be taken
+
+scan(_Json, _Whatever) ->
+    lager:info("GOT TO scan!!!!"),
+    ok.
+
+locate(_Json, _Whatever) ->
+    lager:info("GOT TO locate!!!!"),
+    ok.
+
+query(_Json, _Whatever) ->
+    lager:debug("GOT TO query!!!!"),
+    ok.
+
+report(_Json, _Whatever) ->
+    lager:debug("GOT TO report!!!!"),
+    ok.
+
+get(_Json, _Whatever) ->
+    lager:debug("GOT TO get!!!!"),
+    ok.
+
+notify(_Json, _Whatever) ->
+    lager:debug("GOT TO notify!!!!"),
+    ok.
+
+deny(_Json, _Whatever) ->
+    lager:debug("GOT TO deny!!!!"),
+    ok.
+
+contain(_Json, _Whatever) ->
+    lager:debug("GOT TO contain!!!!"),
+    ok.
+
+allow(_Json, _Whatever) ->
+    lager:debug("GOT TO allow!!!!"),
+    ok.
+
+start(_Json, _Whatever) ->
+    lager:debug("GOT TO start!!!!"),
+    ok.
+
+stop(_Json, _Whatever) ->
+    lager:debug("GOT TO stop!!!!"),
+    ok.
+
+restart(_Json, _Whatever) ->
+    lager:debug("GOT TO restart!!!!"),
+    ok.
+
+pause(_Json, _Whatever) ->
+    lager:debug("GOT TO pause!!!!"),
+    ok.
+
+resume(_Json, _Whatever) ->
+    lager:debug("GOT TO resume!!!!"),
+    ok.
+
+cancel(_Json, _Whatever) ->
+    lager:debug("GOT TO cancel!!!!"),
+    ok.
+
+set(_Json, _Whatever) ->
+    lager:debug("GOT TO set!!!!"),
+    ok.
+
+update(_Json, _Whatever) ->
+    lager:debug("GOT TO update!!!!"),
+    ok.
+
+move(_Json, _Whatever) ->
+    lager:debug("GOT TO move!!!!"),
+    ok.
+
+redirect(_Json, _Whatever) ->
+    lager:debug("GOT TO redirect!!!!"),
+    ok.
+
+delete(_Json, _Whatever) ->
+    lager:debug("GOT TO delete!!!!"),
+    ok.
+
+snapshot(_Json, _Whatever) ->
+    lager:debug("GOT TO snapshot!!!!"),
+    ok.
+
+detonate(_Json, _Whatever) ->
+    lager:debug("GOT TO detonate!!!!"),
+    ok.
+
+restore(_Json, _Whatever) ->
+    lager:debug("GOT TO restore!!!!"),
+    ok.
+
+save(_Json, _Whatever) ->
+    lager:debug("GOT TO save!!!!"),
+    ok.
+
+modify(_Json, _Whatever) ->
+    lager:debug("GOT TO modify!!!!"),
+    ok.
+
+throttle(_Json, _Whatever) ->
+    lager:debug("GOT TO throttle!!!!"),
+    ok.
+
+delay(_Json, _Whatever) ->
+    lager:debug("GOT TO delay!!!!"),
+    ok.
+
+substitute(_Json, _Whatever) ->
+    lager:debug("GOT TO substitute!!!!"),
+    ok.
+
+copy(_Json, _Whatever) ->
+    lager:debug("GOT TO copy!!!!"),
+    ok.
+
+sync(_Json, _Whatever) ->
+    lager:debug("GOT TO sync!!!!"),
+    ok.
+
+distill(_Json, _Whatever) ->
+    lager:debug("GOT TO distill!!!!"),
+    ok.
+
+augment(_Json, _Whatever) ->
+    lager:debug("GOT TO augment!!!!"),
+    ok.
+
+investigate(_Json, _Whatever) ->
+    lager:debug("GOT TO investigate!!!!"),
+    ok.
+
+mitigate(_Json, _Whatever) ->
+    lager:info("Got to mitigate!!!!"),
+    %% what happens next? 
+    %%     spin up a process for this command and have it orchestrate
+    %%     have it spin up a process for target & actuator (or should actuators already be up?)
+    ok.
+
+remediate(_Json, _Whatever) ->
+    lager:debug("GOT TO remediate!!!!"),
+    ok.
+
+response(_Json, _Whatever) ->
+    lager:debug("GOT TO response!!!!"),
+    ok.
+
+alert(_Json, _Whatever) ->
+    lager:debug("GOT TO alert!!!!"),
+    ok.
+
--- apps/ocas/src/openc2_handler.erl
+++ apps/ocas/src/openc2_handler.erl
@@ -50,11 +50,11 @@ init( {tcp, http}, _Req, _Opts) ->
 rest_init(Req, _Opts) ->
     {Method, Req1} = cowboy_req:method(Req),
     {URL, Req2} = cowboy_req:url(Req1),
-    lager:debug("~s ~s", [Method, URL]),
+    lager:info("rest_init:~s ~s", [Method, URL]),
     {ok, Req2, #{}}.
 
 allowed_methods(Req, State) ->
-    %%lager:debug("got to allowed methods"),
+    lager:info("got to allowed methods"),
     {[<<"POST">>], Req, State}.
 
 %%resource_exists(Req, State) ->
@@ -64,243 +64,88 @@ allowed_methods(Req, State) ->
 %%    {false, Req, State}.
 
 content_types_accepted(Req, State) ->
-    %%lager:debug("got to content_types"),
+    lager:info("got to content_types"),
     %% header has content =application/json/whatever
     { [{ { <<"application">>, <<"json">>, '*'} , handle_json}], Req, State}.
 
 handle_json(Req, State) ->
-    %%lager:debug("got to handle_json"),
+    lager:info("got to handle_json"),
     %% put stuff here for actually doing stuff
 
     %% handle case of no body
     HasBody = cowboy_req:has_body(Req),
     Req1 = respond_json(HasBody, Req),
 
-    lager:debug("got past resond_json"),
+    lager:info("got past resond_json"),
     {true, Req1, State}.
 
 %% respond to not having the necessary json body
 respond_json(false, Req) ->
-    lager:debug("got to respond_json false"),
+    lager:info("got to respond_json false"),
     cowboy_req:reply(400, [], <<"Missing body.">>, Req);
 
 %% respond when there is a body
 respond_json(true, Req) ->
-    %%lager:debug("got to respond_json true"),
+    lager:info("got to respond_json true"),
 
     %% get the body of the request
     { ok, Body, Req1} = cowboy_req:body(Req),
 
     %% decode the json into a map of erlang terms
     JsonMap = jsx:decode(Body, [return_maps]),
-    %%lager:debug("handle_json JsonInputMap ~p", [JsonMap] ),
+    lager:info("handle_json JsonInputMap ~p", [JsonMap] ),
 
     %% verify json - add this in eventually
     %% put stuff here
+    %% verify by checking if routing exists
+    %%     convert action to atom
+    %%     verify routine exists
+    lager:info("about to verify_action!!!!"),
+    ActionKeyExists = maps:is_key( <<"action">>, JsonMap ),
+    lager:info("ActionKeyExists: ~p!!!!", [ActionKeyExists]),
+    VerfiedAction = verify_action(ActionKeyExists,JsonMap),    
+    lager:info("return verify_action: ~p!!!!", [VerfiedAction]),
+
 
     %% separate out the commands and deal with each - add this in eventually
     %% put stuff here
     What = json_actions(JsonMap),
+    lager:info("json_actions(JsonMap): ~p!!!!", [What]),
     
     %% for now just reply with what came in as plain text
     ReplyBody = jsx:encode(JsonMap, [{indent,2}]),
     Headers = [ {<<"content-type">>, <<"text/plain; charset=utf-8">>} ],
     %% put together the reply and return it
-    lager:debug("about to do reply in respond json"),
+    lager:info("about to do reply in respond json"),
     cowboy_req:reply(200, Headers, ReplyBody, Req1).
     
 
+verify_action( false, JsonMap ) ->
+  %% false means no action key - input json was bad
+  lager:info("verify_action failed - no action in json input"),
+  invalid_json_no_action_keyword;
+
+verify_action( true, JsonMap ) ->
+  %% action key exists, get value and verify
+  lager:info("verify_action has action key"),
+
+  %% key value for Action Key
+  #{ <<"action">> := ActionBin } = JsonMap,
+  lager:info("action bintext: ~p", [ActionBin] ),
+  %% value is binary text, routines are atoms
+  %% one or other needs to be converted
+  %% logical might be to convert input to atom using
+  %% binary_to_atom - but that leaves routine open to
+  %% DoS attack by flooding with invalid actions
+  %% instead convert routine names to binary text
+  ValidAction = actions:get_valid_action(ActionBin),
+  lager:info("action atom: ~p", [ValidAction] ),
+
+  %% return valid action
+  ValidAction.
+
 %% do different things for the different actions (and fail if action not found)
 
-%%     match on scan
-json_actions(#{ <<"action">> := <<"scan">> } ) ->
-    lager:debug("GOT TO scan!!!!"),
-    ok;
-
-%%     match on locate
-json_actions(#{ <<"action">> := <<"locate">> } ) ->
-    lager:debug("GOT TO locate!!!!"),
-    ok;
-
-%%     match on query
-json_actions(#{ <<"action">> := <<"query">> } ) ->
-    lager:debug("GOT TO query!!!!"),
-    ok;
-
-%%     match on report
-json_actions(#{ <<"action">> := <<"report">> } ) ->
-    lager:debug("GOT TO report!!!!"),
-    ok;
-
-%%     match on get
-json_actions(#{ <<"action">> := <<"get">> } ) ->
-    lager:debug("GOT TO get!!!!"),
-    ok;
-
-%%     match on notify
-json_actions(#{ <<"action">> := <<"notify">> } ) ->
-    lager:debug("GOT TO notify!!!!"),
-    ok;
-
-%%     match on deny
-json_actions(#{ <<"action">> := <<"deny">> } ) ->
-    lager:debug("GOT TO deny!!!!"),
-    ok;
-
-%%     match on contain
-json_actions(#{ <<"action">> := <<"contain">> } ) ->
-    lager:debug("GOT TO contain!!!!"),
-    ok;
-
-%%     match on allow
-json_actions(#{ <<"action">> := <<"allow">> } ) ->
-    lager:debug("GOT TO allow!!!!"),
-    ok;
-
-%%     match on start
-json_actions(#{ <<"action">> := <<"start">> } ) ->
-    lager:debug("GOT TO start!!!!"),
-    ok;
-
-%%     match on stop
-json_actions(#{ <<"action">> := <<"stop">> } ) ->
-    lager:debug("GOT TO stop!!!!"),
-    ok;
-
-%%     match on restart
-json_actions(#{ <<"action">> := <<"restart">> } ) ->
-    lager:debug("GOT TO restart!!!!"),
-    ok;
-
-%%     match on pause
-json_actions(#{ <<"action">> := <<"pause">> } ) ->
-    lager:debug("GOT TO pause!!!!"),
-    ok;
-
-%%     match on resume
-json_actions(#{ <<"action">> := <<"resume">> } ) ->
-    lager:debug("GOT TO resume!!!!"),
-    ok;
-
-%%     match on cancel
-json_actions(#{ <<"action">> := <<"cancel">> } ) ->
-    lager:debug("GOT TO cancel!!!!"),
-    ok;
-
-%%     match on set
-json_actions(#{ <<"action">> := <<"set">> } ) ->
-    lager:debug("GOT TO set!!!!"),
-    ok;
-
-%%     match on update
-json_actions(#{ <<"action">> := <<"update">> } ) ->
-    lager:debug("GOT TO update!!!!"),
-    ok;
-
-%%     match on move
-json_actions(#{ <<"action">> := <<"move">> } ) ->
-    lager:debug("GOT TO move!!!!"),
-    ok;
-
-%%     match on redirect
-json_actions(#{ <<"action">> := <<"redirect">> } ) ->
-    lager:debug("GOT TO redirect!!!!"),
-    ok;
-
-%%     match on delete
-json_actions(#{ <<"action">> := <<"delete">> } ) ->
-    lager:debug("GOT TO delete!!!!"),
-    ok;
-
-%%     match on snapshot
-json_actions(#{ <<"action">> := <<"snapshot">> } ) ->
-    lager:debug("GOT TO snapshot!!!!"),
-    ok;
-
-%%     match on detonate
-json_actions(#{ <<"action">> := <<"detonate">> } ) ->
-    lager:debug("GOT TO detonate!!!!"),
-    ok;
-
-%%     match on restore
-json_actions(#{ <<"action">> := <<"restore">> } ) ->
-    lager:debug("GOT TO restore!!!!"),
-    ok;
-
-%%     match on save
-json_actions(#{ <<"action">> := <<"save">> } ) ->
-    lager:debug("GOT TO save!!!!"),
-    ok;
-
-%%     match on modify
-json_actions(#{ <<"action">> := <<"modify">> } ) ->
-    lager:debug("GOT TO modify!!!!"),
-    ok;
-
-%%     match on throttle
-json_actions(#{ <<"action">> := <<"throttle">> } ) ->
-    lager:debug("GOT TO throttle!!!!"),
-    ok;
-
-%%     match on delay
-json_actions(#{ <<"action">> := <<"delay">> } ) ->
-    lager:debug("GOT TO delay!!!!"),
-    ok;
-
-%%     match on substitute
-json_actions(#{ <<"action">> := <<"substitute">> } ) ->
-    lager:debug("GOT TO substitute!!!!"),
-    ok;
-
-%%     match on copy
-json_actions(#{ <<"action">> := <<"copy">> } ) ->
-    lager:debug("GOT TO copy!!!!"),
-    ok;
-
-%%     match on sync
-json_actions(#{ <<"action">> := <<"sync">> } ) ->
-    lager:debug("GOT TO sync!!!!"),
-    ok;
-
-%%     match on distill
-json_actions(#{ <<"action">> := <<"distill">> } ) ->
-    lager:debug("GOT TO distill!!!!"),
-    ok;
-
-%%     match on augment
-json_actions(#{ <<"action">> := <<"augment">> } ) ->
-    lager:debug("GOT TO augment!!!!"),
-    ok;
-
-%%     match on investigate
-json_actions(#{ <<"action">> := <<"investigate">> } ) ->
-    lager:debug("GOT TO investigate!!!!"),
-    ok;
-
-%%     match on mitigate
-json_actions(#{ <<"action">> := <<"mitigate">> } ) ->
-    lager:debug("GOT TO mitigate!!!!"),
-    ok;
-
-%%     match on remediate
-json_actions(#{ <<"action">> := <<"remediate">> } ) ->
-    lager:debug("GOT TO remediate!!!!"),
-    ok;
-
-%%     match on response
-json_actions(#{ <<"action">> := <<"response">> } ) ->
-    lager:debug("GOT TO response!!!!"),
-    ok;
-
-%%     match on alert
-json_actions(#{ <<"action">> := <<"alert">> } ) ->
-    lager:debug("GOT TO alert!!!!"),
-    ok;
-
-%%     match on scan
-json_actions(#{ <<"action">> := <<"scan">> } ) ->
-    lager:debug("GOT TO scan!!!!"),
-    ok;
 
 json_actions(JsonMap) ->
     %% something is wrong!!! Bad input json with unrecognized action
@@ -308,3 +153,8 @@ json_actions(JsonMap) ->
     #{ <<"action">> := Action} = JsonMap,
    got_bad_action = Action,  %% should blow up here - fix later to respond with bad input
     ok.
+
+get_valid_actions() ->
+  ActionList = actions:module_info(functions),
+  lager:info("get_valid_actions-valid= ~p", [ActionList] ),
+  ActionList.
